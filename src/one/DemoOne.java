d3fc90dffbfffdae71b2494cf5cce030f2bd4d77
Mihail <Mihail@LAPTOP-DTKS6POR.fios-router.home>
2021-11-24 20:39:04 -0500

    Commit ...

---
+++ src/one/DemoOne.java
@@ -0,0 +1,12 @@
+package one;
+
+public class DemoOne {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Demo One...");
+		
+		System.out.println("Random Change...");
+	}
+	
+}
